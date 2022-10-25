907f493da5e2af1b4303bd8ee41f0b476b2afc5a
Michael Utz <michael@theutz.com>
2022-10-25 16:31:41 +0300

    Add some basic text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ __('Laravel Unité') }}</title>
     @vite('resources/css/app.css')
 </head>
 
 <body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="container mx-auto p-8">
+        <h1 class="text-3xl font-bold underline">
+            {{ __('Laravel Unité') }}
+        </h1>
+        <h2 class="text-xl mt-8">
+            {{ __('A unified toolkit for working with units in Laravel.') }}
+        </h2>
+    </div>
 </body>
 
 </html>
